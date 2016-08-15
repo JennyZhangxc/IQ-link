e2de57114c98265a9612a12238d9cd6c1fea9ee1
Wei <u6137273@anu.edu.au>
2016-08-15 14:01:29 +1000

    modify class Main.

--- src/comp1110/ass2/gittest/Main.java
+++ src/comp1110/ass2/gittest/Main.java
@@ -3,8 +3,10 @@ package comp1110.ass2.gittest;
 /**
  * Created by huanglei on 16/8/15.
  */
-public class Main {
-    public static void main(String[] args) {
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        B b = new B();
     }
 }

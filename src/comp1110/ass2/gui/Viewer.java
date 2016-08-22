8a36792a724014dd0c23f38addcd31dac8605f28
Lei Huang <u6041747@anu.edu.au>
2016-08-22 16:03:23 +1000

    Deliverable D2C done.

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -1,5 +1,7 @@
 package comp1110.ass2.gui;
 
+import com.sun.xml.internal.stream.Entity;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,8 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A very simple viewer for piece placements in the link game.
@@ -26,6 +34,7 @@ public class Viewer extends Application {
     private static final double ROW_HEIGHT = SQUARE_SIZE * 0.8660254; // 60 degrees
     private static final int VIEWER_WIDTH = 750;
     private static final int VIEWER_HEIGHT = 500;
+    private boolean Flag = false;
 
     private static final String URI_BASE = "assets/";
 
@@ -39,8 +48,60 @@ public class Viewer extends Application {
      *
      * @param placement  A valid placement string
      */
+
     void makePlacement(String placement) {
         // FIXME Task 5: implement the simple placement viewer
+        int length=placement.length()/3;
+        if(length%3==0){
+            Flag = true;
+        }
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<length;i++){
+            list.add(placement.substring(i*3,(i+1)*3));
+        }
+        List<Integer> index = new ArrayList<>();
+        for (int j = 0; j < list.size(); j++) {
+            index.add(Character.getNumericValue(list.get(j).charAt(0)) - 10);
+
+        }
+        int[][] position = new int[length][2];
+
+        for(int i=0;i<length;i++){
+            position[i][0]=index.get(i)/6;//row
+            position[i][1]=index.get(i)%6;//column
+
+        }
+
+        try {
+            Pane pane =new Pane();
+            for (int i = 0; i < length; i++) {
+                char piece=list.get(i).charAt(1);
+                Image image = new Image("file:src/comp1110/ass2/gui/assets/"+piece+".png");
+                ImageView iv1 = new ImageView();
+                iv1.setImage(image);
+                iv1.setFitHeight(PIECE_IMAGE_SIZE);
+                iv1.setFitWidth(PIECE_IMAGE_SIZE);
+                if(position[i][0]%2==0){
+                    iv1.relocate(position[i][1]*SQUARE_SIZE-SQUARE_SIZE/2,position[i][0]*ROW_HEIGHT-SQUARE_SIZE);
+                }
+                else{
+                    iv1.relocate(position[i][1]*SQUARE_SIZE,position[i][0]*ROW_HEIGHT-SQUARE_SIZE);
+                }
+                char orientation =list.get(i).charAt(2);
+                int angle = (Character.getNumericValue(orientation)-10)*60;
+                if(angle>360){
+                    iv1.setScaleY(-1);
+                }
+                iv1.setRotate(angle);
+                pane.getChildren().add(iv1);
+            }
+
+            root.getChildren().add(pane);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
 
     }
 
@@ -75,6 +136,7 @@ public class Viewer extends Application {
 
         root.getChildren().add(controls);
 
+        makePlacement("BAAHBATCJRDKWEB");
         makeControls();
 
         primaryStage.setScene(scene);
